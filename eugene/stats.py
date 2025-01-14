ab47e460742a27d6c700e264d6dd5c00d47baaef
Eugene <eugene@3bytesahead.com>
2025-01-14 16:33:50 +0100

    more work

---
+++ eugene/stats.py
@@ -0,0 +1,161 @@
+import jax, time, pickle
+
+class Stats:
+    class c:
+        CEND    = '\33[0m'
+        CBLACK  = '\33[30m'
+        CRED    = '\33[31m'
+        CGREEN  = '\33[32m'
+        CYELLOW = '\33[33m'
+        CBLUE   = '\33[34m'
+        CVIOLET = '\33[35m'
+        CBEIGE  = '\33[36m'
+        CWHITE  = '\33[37m'
+
+        CYCLE = [CRED, CGREEN, CYELLOW, CBLUE, CVIOLET, CBEIGE, CWHITE]
+
+    class Time():
+        @classmethod
+        def block(cls, val):
+            if type(val).__name__ in ['FrozenDict', 'dict']:
+                jax.tree_util.tree_map(lambda x: x.block_until_ready(), val)
+            else:
+                val.block_until_ready()
+            return val
+        
+        def __init__(self, key, on_complete_func, **kwargs):
+            self.key = key
+            self.on_complete_func = on_complete_func
+            self.kwargs = kwargs
+
+        def __enter__(self):
+            self.t0 = time.time()
+            return self.block
+
+        def __exit__(self, *args):
+            self.on_complete_func(time.time() - self.t0)
+
+            if self.kwargs.get('print', False):
+                print(*[f"{Stats.c.CBLACK}{self.key}", Stats.c.CEND, "\t\t", f'{time.time() - self.t0:.3f}s'])
+
+    def time(self, key, **kwargs):
+        def on_complete(dt):
+            for k, v in key.items():
+                if not isinstance(v, set):
+                    continue
+
+                self.write({k: {next(iter(v)): dt}}, self.dict, **kwargs)
+            
+        return Stats.Time(key, on_complete, **kwargs)
+
+    def __init__(self, filename = None):
+        self.dict = {}
+        self.filename = filename
+
+    def write(self, key_dict_or_str, dict_, **kwargs):
+        for k, v in key_dict_or_str.items():
+            if isinstance(v, dict):
+                if not k in dict_:
+                    dict_[k] = {}
+
+                self.write(v, dict_[k], **kwargs)
+            else:
+                self.write_leaf(k, v, dict_, **kwargs)
+
+    def write_leaf(self, key, value, dict_, append = True, **kwargs):
+        if append is not True:
+            dict_[key] = value
+        elif not key in dict_:
+            dict_[key] = [value]
+        else:
+            dict_[key].append(value)
+    
+    def __call__(self, key, **kwrags):
+        self.write(key, self.dict, **kwrags)
+        return self
+        
+    def __repr__(self) -> str:
+        return f"Stats{self.dict.__repr__()}"
+    
+    def __getitem__(self, key):
+        return self.dict[key]
+
+    # def __getattr__(self, name):
+    #     print(name)
+    #     # call the method on the dict if method
+    #     # does not exist on the Stats object
+    #     if hasattr(self, name):
+    #         return getattr(self, name)
+        
+    #     return getattr(self.dict, name)
+
+    def latest(self, *vals, **kwargs):
+        acc = []
+        for i, val in enumerate(vals):
+            i -= 1
+            if type(val) is str:
+                acc.append(f"{Stats.c.CBLACK}{val}{Stats.c.CEND}")
+            elif type(val) is dict:
+                c = Stats.c.CYCLE[i % len(Stats.c.CYCLE)]
+                for i, (k, v) in enumerate(val.items()):
+                    if type(v) is dict: raise Exception("Implement me!")
+                    if type(v) is str: v = [v]
+
+                    v = self.dict[k].keys() if v == ['*'] else v
+                    a = [f"{c}{k}:["]
+                    a.append(' '.join([f"{c}{vv}: {Stats.c.CWHITE}{self.dict[k][vv][-1]:.3f}{Stats.c.CEND}" for vv in v]))
+                    a.append(f"{c}]{Stats.c.CEND}")
+                    acc.append(''.join(a))
+            else:
+                acc.append(f"<unknown type>")
+        
+        return acc
+
+    def persist(self):
+        if not self.filename:
+            print("*** NOT PERSISTED: No filename provided! ***")
+            return None
+
+        with open(self.filename, 'wb') as file:
+            pickle.dump(self, file)
+
+    def load(self):
+        if not self.filename:
+            raise Exception("No filename provided!")
+
+        with open(self.filename, 'rb') as file:
+            self.dict = pickle.load(file).dict
+        
+        return self
+
+if __name__ == '__main__':
+    stats = Stats('tmp/stats-demo.pkl')
+
+    stats({'top_lvl_append': 32.0})
+    stats({'top_lvl_append': 64.0})
+
+    stats({'args': {'this': 'that'}}, append=False)
+    stats({'train': {'loss': 11}})
+    stats({'train': {'loss': 10}})
+    stats({'train': {'loss': 9}})
+    stats({'train': {'acc': 5}})
+    stats({'train': {'acc': 6}})
+    stats({'train': {'acc': 7}})
+    stats({'test': {'hello': 123}})
+    
+    with stats.time({'time': {'load_data'}}, append=False) as block:
+        time.sleep(0.256)
+
+    for i in range(8):
+        with stats.time({'time': {'train'}}) as block:
+            time.sleep(0.032)
+        
+    print(*stats.latest(*[
+        f"Stats demo {stats['time']['load_data']:.3f}s",
+        f"{stats['time']['train'][-1]:.3f}",
+        {'train': '*'}
+    ]))
+
+    stats.persist()
+
+    stats = Stats('tmp/stats-demo.pkl').load()
--- eugene/vae.py
+++ eugene/vae.py
@@ -2,15 +2,21 @@ import jax, optax, sys, time, pickle, builtins, os
 import pandas as pd
 import numpy as np
 from jax.numpy import *
+from jax.random import permutation
 from flax import nnx
+from dataclasses import dataclass, fields
+from eugene.stats import Stats
 
 # os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] = 'false'
 
 if 'train' not in locals():
     train_fname = "/data/celeba/celeba_train_images.npy"
     test_fname = "/data/celeba/celeba_test_images.npy"
-    train = np.load(train_fname, mmap_mode='r')
-    train = jax.device_put(train)
+    train = np.load(train_fname)#, mmap_mode='r')
+    # train = train[:2500]
+
+    # train = jax.device_put(train)
+
     # test  = np.load(test_fname, mmap_mode='r')
     # test  = jax.device_put(test)
 
@@ -46,8 +52,19 @@ class ResizeAndConv(nnx.Module):
         return x
 
 class VAE(nnx.Module):
-    def __init__(self, *, rngs: nnx.Rngs):
-        z_dim = 32
+    @dataclass
+    class DefaultOpts:
+        epochs: int = 256       # Number of epochs to train for
+        bs: int = 64            # batch size
+        lr: float = 1e-5        # learnig rate
+        dz: int = 96            # latent dimensionality
+        opt: str = 'adam'       # 'adam'
+        beta: int = 1.0         # \beta-VAE thing
+        
+    def __init__(self, opts = {}, *, rngs: nnx.Rngs):
+        self.stats = Stats()
+        self.opts = self.DefaultOpts(**opts)
+        z_dim = self.opts.dz
 
         self.rngs = rngs
         self.encoder = nnx.Sequential(
@@ -59,8 +76,6 @@ class VAE(nnx.Module):
         
         self.enc_mu = nnx.Linear(32*32*128, z_dim, rngs=rngs)
         self.enc_logvar = nnx.Linear(32*32*128, z_dim, rngs=rngs)
-
-        # self.fc_dec = 
         
         self.fc_dec = nnx.Sequential(
             nnx.Linear(z_dim, 4*4*512, rngs=rngs),
@@ -79,19 +94,104 @@ class VAE(nnx.Module):
             ResizeAndConv(128, 3,   (4, 4), (1, 1), rngs=rngs),
         )
 
-    def __call__(self, x):
+    def reparametrize(self, mu, logvar):
+        # if self.opts.beta == 0.0:
+        #     return mu
+        
+        std = jax.random.normal(self.rngs.reparam(), (mu.shape[0], mu.shape[1]))
+        return mu + exp(0.5 * logvar)* std
+
+    def __call__(self, x, reparam = True):
         x = self.encoder(x)
         x = x.reshape(x.shape[0], -1)
-        mu = self.enc_mu(x)
-        logvar = self.enc_logvar(x)
+        z_mu = self.enc_mu(x)
+        z_logvar = self.enc_logvar(x)
+
+        z = self.reparametrize(z_mu, z_logvar) if reparam else z_mu
+        x_dec = self.decode(z)
         
-        x_dec = self.fc_dec(mu)
+        return x_dec, z_mu, z_logvar
+    
+    def decode(self, z):
+        x_dec = self.fc_dec(z)
         x_dec = x_dec.reshape(x_dec.shape[0], 8, 8, 128)
         x_dec = self.decoder(x_dec)
 
-        # breakpoint()
-
-        return mu, logvar, x_dec
+        return x_dec
+        
     
-vae = VAE(rngs=nnx.Rngs(jax.random.PRNGKey(0)))
-z = vae(train[:1])
+    def dump(self, path):
+        with open(path, 'wb') as file:
+            pickle.dump({
+                'opts':   self.opts, 
+                'stats':  self.stats,
+                'state':  nnx.state(self)
+            }, file)
+
+def loss_fn(model, batch):
+    x_hat, z_mu, z_logvar = model(batch)
+    
+    rec_loss = optax.l2_loss(x_hat, batch).sum([-1,-2,-3])
+    kl_loss = -0.5 * sum(1.0 + z_logvar - z_mu**2 - exp(z_logvar), axis=-1)
+    
+    loss = rec_loss + model.opts.beta * kl_loss
+    
+    stats = {
+        'elbo':    -loss.mean(),
+        'kl_loss':  kl_loss.mean(),
+        'rec_loss': rec_loss.mean(),
+    }
+    
+    return loss.mean(), ((x_hat, z_mu, z_logvar), stats)
+
+@nnx.jit
+def train_epoch(model, optimizer, train):
+    # t0 = time.time()
+    n_full  = train.shape[0] // model.opts.bs
+    permut  = permutation(model.rngs.permut(), n_full * model.opts.bs)
+    batches = train[permut].reshape(n_full, model.opts.bs, *train.shape[1:]) 
+    # print(f"train_epoch permut: {time.time() - t0:.3f}s")
+    return train_epoch_inner(model, optimizer, batches)
+
+@nnx.jit
+def train_epoch_inner(model, optimizer, batches):
+    grad_loss_fn = nnx.value_and_grad(loss_fn, has_aux=True)
+
+    def train_step(model_opt, batch):
+        model, optimizer = model_opt
+        (loss_, (artfcs_, stats)), grads = grad_loss_fn(model, batch)
+        optimizer.update(grads)
+        return (model, optimizer), stats
+    
+    in_axes = (nnx.Carry, 0)
+    train_step_scan_fn = nnx.scan(train_step, in_axes=in_axes)
+    model_opt = (model, optimizer)
+    _, stats_stack = nnx.jit(train_step_scan_fn)(model_opt, batches)
+    return jax.tree.map(lambda x: x.mean(), stats_stack)
+
+if __name__ == "__main__":
+    model = VAE(rngs=nnx.Rngs(jax.random.PRNGKey(0)))
+    params = nnx.state(model, nnx.Param, ...)[0]
+    param_count = builtins.sum(x.size for x in jax.tree_util.tree_leaves(params))
+    print(f"JAXVAE Number of parameters: {param_count // 1e6}M")
+
+    _loss, (_artfcs, _stats) = loss_fn(model, train[:32])
+    print(f"Loss (smoketest): {_loss}")
+
+    tx = getattr(optax, model.opts.opt)(model.opts.lr)
+    optimizer = nnx.Optimizer(model, tx)
+
+    for epoch_idx in range(model.opts.epochs):
+        with model.stats.time({'time': {'forward_train_epoch'}}, print=0) as block:
+            model.train()
+            stats = train_epoch(model, optimizer, train)
+            # model.eval()
+            # stats['oracle'] = oracle_test(model, data)
+            model.stats({'train': jax.tree.map(lambda x: x.item(), stats)})
+        
+        print(*model.stats.latest(*[
+            f"VAE {epoch_idx:03d} {model.stats['time']['forward_train_epoch'][-1]:.3f}s",
+            {'train': '*'}, 
+        ]))
+
+    model.dump(f"eugene/latest.pickle")
