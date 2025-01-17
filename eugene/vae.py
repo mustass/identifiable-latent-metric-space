cbd1600239ccd00547b2a13a0133142e44b77986
Eugene <eugene@3bytesahead.com>
2025-01-17 16:20:54 +0100

    new conv arch

--- eugene/vae.py
+++ eugene/vae.py
@@ -28,21 +28,22 @@ class ResizeAndConv(nnx.Module):
     This block is useful to avoid checkerboard artifacts: https://distill.pub/2016/deconv-checkerboard/
     """
 
-    def __init__(self, in_channels, filters, kernel_size, stride, rngs):
+    def __init__(self, in_channels, filters, kernel_size, strides, padding, rngs):
         self.in_channels = in_channels
         self.filters = filters
         self.kernel_size = kernel_size
-        self.stride = stride
-        self.conv = nnx.Conv(self.in_channels, self.filters, self.kernel_size, (1, 1), rngs=rngs)
+        self.strides = strides
+        self.padding = padding
+        self.conv = nnx.Conv(self.in_channels, self.filters, self.kernel_size, self.padding, rngs=rngs)
 
     def __call__(self, x):
-        if self.stride != (1, 1):
+        if self.strides != (1, 1):
             x = jax.image.resize(
                 x,
                 (
                     x.shape[0],
-                    x.shape[1] * self.stride[0],
-                    x.shape[2] * self.stride[1],
+                    x.shape[1] * self.strides[0],
+                    x.shape[2] * self.strides[1],
                     x.shape[3],
                 ),
                 method="nearest",
@@ -66,25 +67,22 @@ class VAE(nnx.Module):
         def __init__(self, opts, rngs):
             self.opts = opts
             self.fc_dec = nnx.Sequential(
-                nnx.Linear(opts.dz, 8*8*32, rngs=rngs), # nnx.Linear(opts.dz, 4*4*512, rngs=rngs),
-                nnx.elu
+                nnx.Linear(opts.dz, 512*4, rngs=rngs), nnx.elu
             )
 
             self.convs = nnx.Sequential(
-                ResizeAndConv(32, 128, (4, 4), (1, 1), rngs=rngs), # ResizeAndConv(128, 128, (4, 4), (1, 1), rngs=rngs),
-                nnx.elu,
-                ResizeAndConv(128, 128, (4, 4), (2, 2), rngs=rngs),
-                nnx.elu,
-                ResizeAndConv(128, 128, (4, 4), (2, 2), rngs=rngs),
-                nnx.elu,
-                ResizeAndConv(128, 128, (4, 4), (2, 2), rngs=rngs),
-                nnx.elu,
-                ResizeAndConv(128, 3,   (4, 4), (1, 1), rngs=rngs),
+                ResizeAndConv(512, 256, kernel_size=(3, 3), strides=(1, 1), padding=1, rngs=rngs), nnx.elu,
+                ResizeAndConv(256, 128, kernel_size=(3, 3), strides=(2, 2), padding=1, rngs=rngs), nnx.elu,
+                ResizeAndConv(128, 64,  kernel_size=(3, 3), strides=(2, 2), padding=1, rngs=rngs), nnx.elu,
+                ResizeAndConv(64,  32,  kernel_size=(3, 3), strides=(2, 2), padding=1, rngs=rngs), nnx.elu,
+                ResizeAndConv(32,  16,  kernel_size=(3, 3), strides=(2, 2), padding=1, rngs=rngs), nnx.elu,
+                ResizeAndConv(16,  8,   kernel_size=(3, 3), strides=(2, 2), padding=1, rngs=rngs), nnx.elu,
+                ResizeAndConv(8,   3,   kernel_size=(3, 3), strides=(1, 1), padding=1, rngs=rngs),
             )
         
         def __call__(self, z):
             x_dec = self.fc_dec(z)
-            x_dec = x_dec.reshape(x_dec.shape[0], 8, 8, 32) #.reshape(x_dec.shape[0], 8, 8, 128)
+            x_dec = x_dec.reshape(x_dec.shape[0], 2, 2, 512)
             x_dec = self.convs(x_dec)
             return x_dec 
         
@@ -95,14 +93,15 @@ class VAE(nnx.Module):
 
         self.rngs = rngs
         self.encoder = nnx.Sequential(
-            nnx.Conv(3, 128, kernel_size=(4, 4), strides=(1, 1), rngs=rngs),
-            nnx.elu,
-            nnx.Conv(128, 128, kernel_size=(4, 4), strides=(2, 2), rngs=rngs),
-            nnx.elu,
+            nnx.Conv(3, 32,    kernel_size=(3, 3), strides=2, padding=1, rngs=rngs), nnx.elu,
+            nnx.Conv(32, 64,   kernel_size=(3, 3), strides=2, padding=1, rngs=rngs), nnx.elu,
+            nnx.Conv(64, 128,  kernel_size=(3, 3), strides=2, padding=1, rngs=rngs), nnx.elu,
+            nnx.Conv(128, 256, kernel_size=(3, 3), strides=2, padding=1, rngs=rngs), nnx.elu,
+            nnx.Conv(256, 512, kernel_size=(3, 3), strides=2, padding=1, rngs=rngs), nnx.elu,
         )
         
-        self.enc_mu = nnx.Linear(32*32*128, z_dim, rngs=rngs)
-        self.enc_logvar = nnx.Linear(32*32*128, z_dim, rngs=rngs)
+        self.enc_mu = nnx.Linear(2*2*512, z_dim, rngs=rngs)
+        self.enc_logvar = nnx.Linear(2*2*512, z_dim, rngs=rngs)
         
         rngss = nnx.vmap(lambda s: nnx.Rngs(s), in_axes=0)(split(rngs(), self.opts.nD))
         self.decoder = nnx.vmap(self.Decoder, in_axes=(None, 0))(self.opts, rngss)
