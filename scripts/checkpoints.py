c47f11fccbdcb8a28f93b111344e4dcfa8df8bdc
Stas Syrota <mustas@pm.me>
2025-01-22 11:23:09 +0100

    checkpoint util

--- scripts/checkpoints.py
+++ scripts/checkpoints.py
@@ -7,6 +7,12 @@ def retrieve_checkpoints(project_dir, model_identifier='best_model.pickle'):
     I want to go through all subfolders of project_dir and check which ones have model_identifier existing. 
     For all success cases, I want to append the path to them to a list []
     """ 
-
+    checkpoints_dir = []
+    checkpoints_files= []
+    for root, dirs, files in os.walk(project_dir):
+        if model_identifier in files:
+            checkpoints_dir.append(root)
+            checkpoints_files.append(os.path.join(root, model_identifier))
+    return checkpoints_dir, checkpoints_files
 
     
