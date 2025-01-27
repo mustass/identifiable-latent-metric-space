731489de38455dfbd5659f2eb457813d13eb0b56
Stas Syrota <stasmagdych@pm.me>
2025-01-27 15:35:27 +0100

    running the geodesics

--- bash_queue/run_geodesics_batch_2.sh
+++ bash_queue/run_geodesics_batch_2.sh
@@ -13,8 +13,8 @@ module load python3/3.10.15 cuda/12.6.3 cudnn/v9.6.0.74-prod-cuda-12.X
 source /work3/s210527/nnx//bin/activate
 export XLA_PYTHON_CLIENT_PREALLOCATE=false
 
-name="celeba_hpc_geodesics_sgd_100"
+name="celeba_hpc_geodesics_sgd_100_batch_2"
 echo $name
-comnd="python3 scripts/inference_geodesics.py general.run_name=${name}"
+comnd="python3 scripts/inference_geodesics.py general.run_name=${name} inference=ensemble_geodesics_batch_2"
 echo $comnd
 eval $comnd
